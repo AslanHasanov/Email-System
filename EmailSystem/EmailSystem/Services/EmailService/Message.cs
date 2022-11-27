28eded59321b5b70176f76bd1b6dba0c4c9914c9
AslanHasanov <aslanrh@code.edu.az>
2022-11-27 15:54:40 +0400

    EmailConfiguration, EmailSender, Message classes added to system

--- EmailSystem/EmailSystem/Services/EmailService/Message.cs
+++ EmailSystem/EmailSystem/Services/EmailService/Message.cs
@@ -1,6 +1,19 @@
-﻿namespace EmailSystem.Services.EmailService
+﻿using MimeKit;
+
+namespace EmailSystem.Services.EmailService
 {
     public class Message
     {
+        public List<MailboxAddress> To { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public Message(List<MailboxAddress> to, string subject, string content)
+        {
+            To = new List<MailboxAddress>();
+
+            To.AddRange(to.Select(x => new MailboxAddress(string.Empty, x.ToString())));
+            Subject = subject;
+            Content = content;
+        }
     }
 }
